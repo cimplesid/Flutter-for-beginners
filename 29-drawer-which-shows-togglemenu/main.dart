47d330f6632f594b208a9908170570782d242bd5
Siddhartha joshi <joshisid178@gmail.com>
2019-12-15 18:53:09 +0545

    29-drawer-which-shows-togglemenu

---
+++ 29-drawer-which-shows-togglemenu/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+//State is information of the application that can change over time or when some actions are taken.
+class _State extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Name here'),
+        backgroundColor: Colors.red,
+      ),
+      drawer: Drawer(
+        child: Container(
+          padding: EdgeInsets.all(32.0),
+          child: Column(
+            children: <Widget>[
+              Text('Hello Drawer'),
+              RaisedButton(
+                  onPressed: () => Navigator.pop(context), child: Text('Close'))
+            ],
+          ),
+        ),
+      ),
+      //hit Ctrl+space in  vscode to know what are the options you can use in flutter widgets
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[Text('Add widgets here')],
+          ),
+        ),
+      ),
+    );
+  }
+}
