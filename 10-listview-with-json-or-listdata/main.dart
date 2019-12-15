df625c189cfd9d5be6e8127da161e416d15ed51d
Siddhartha joshi <joshisid178@gmail.com>
2019-12-15 18:48:01 +0545

    10-listview-with-json-or-listdata

---
+++ 10-listview-with-json-or-listdata/main.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MaterialApp(home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  List data;
+
+  Future<String> getData() async {
+    var response = await http.get(
+        Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"),
+        headers: {"Accept": "application/json"});
+
+    this.setState(() {
+      data = json.decode(response.body);
+    });
+
+    print(data[1]["title"]);
+
+    return "Success!";
+  }
+
+  @override
+  void initState() {
+    this.getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Listviews"), backgroundColor: Colors.blue),
+      body: ListView.builder(
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Text(data[index]["title"]),
+          );
+        },
+      ),
+    );
+  }
+}
