412c19734be8aaa20365e9f90f8bc712d23afc41
Siddhartha joshi <joshisid178@gmail.com>
2019-12-15 18:46:44 +0545

    05-using-onChanged-to-show-input-text

---
+++ 06-using-onSubmitted-to-show-input-text-after-submit/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyTextInput()));
+}
+
+class MyTextInput extends StatefulWidget {
+  @override
+  MyTextInputState createState() => MyTextInputState();
+}
+
+class MyTextInputState extends State<MyTextInput> {
+  String result = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Input Text"), backgroundColor: Colors.deepOrange),
+        body: Container(
+            child: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+              TextField(
+                  decoration: InputDecoration(hintText: "Type in here"),
+                  //onChanged is called whenever we add or delete something on Text Field
+                  onSubmitted: (String str) {
+                    setState(() {
+                      result = str;
+                    });
+                  }),
+              //displaying input text
+              Text(result)
+            ]))));
+  }
+}
