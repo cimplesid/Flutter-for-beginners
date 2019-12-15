644a09d97afdc61ffb339763c454a48d45082a30
Siddhartha joshi <joshisid178@gmail.com>
2019-12-15 19:01:32 +0545

    59-custom-widgets-using-stateful

---
+++ 59-custom-widgets-using-stateful/lib/mywidget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(32.0),
+      child: Text('Hello World'),
+    );
+  }
+}
