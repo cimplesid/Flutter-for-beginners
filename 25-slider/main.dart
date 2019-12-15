cc284cd11315c304072ced317e12a77c866ac15d
Siddhartha joshi <joshisid178@gmail.com>
2019-12-15 18:52:19 +0545

    25-slider

---
+++ 25-slider/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+//State is information of the application that can change over time or when some actions are taken.
+class _State extends State<MyApp> {
+  double _value = 0.0;
+  void _setvalue(double value) => setState(() => _value = value);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Name here'),
+      ),
+      //hit Ctrl+space in  vscode to know what are the options you can use in flutter widgets
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text('Value: ${(_value * 100).round()}'),
+              Slider(value: _value, onChanged: _setvalue)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
