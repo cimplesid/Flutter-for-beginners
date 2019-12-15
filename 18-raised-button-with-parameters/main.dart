9809f20effeaedfda493273d4dac17c15c71a7fd
Siddhartha joshi <joshisid178@gmail.com>
2019-12-15 18:50:24 +0545

    18-raised-button-with-parameters

---
+++ 18-raised-button-with-parameters/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  String _value = 'Hello World';
+
+  void _onPressed(String value) {
+    setState(() {
+      _value = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Name here'),
+      ),
+      //hit Ctrl+space in  vscode to know what are the options you can use in flutter widgets
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(_value),
+              //dart treates everything as objects so we pass a function in onPressed value
+              RaisedButton(
+                  onPressed: () => _onPressed('Hello Raunak'),
+                  child: Text('Click me')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
